a0d97fdee8ea11deef41b0fa10f4322558761278
hyeonic <dev.hyeonic@gmail.com>
2021-12-15 21:47:48 +0900

    feat: InputView 플레이어 이름 입력 시 안내 문구 추가

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -3,11 +3,13 @@ package blackjack.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String INPUT_NAME_MESSAGE = "\n플레이어의 이름을 입력하세요.";
 
     private InputView() {
     }
 
     public static String name() {
+        System.out.println(INPUT_NAME_MESSAGE);
         return Console.readLine();
     }
 }
