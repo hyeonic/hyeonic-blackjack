f507297a96a05f00a6aeada6b26a87167262a061
hyeonic <dev.hyeonic@gmail.com>
2021-12-15 21:53:24 +0900

    feat: OutputView 초기 메시지 및 현재 카드 상태 출력을 위한 메서드 추가

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -1,13 +1,39 @@
 package blackjack.view;
 
-public class OutputView {
+import static java.util.stream.Collectors.*;
 
-    public static final String GUIDE_MESSAGE = "블랙잭 게임을 시작합니다.";
+import java.util.List;
+
+import blackjack.domain.card.Cards;
+import blackjack.domain.player.Name;
+import blackjack.domain.player.Player;
+
+public class OutputView {
+    private static final String GUIDE_MESSAGE = "블랙잭 게임을 시작합니다.";
+    private static final String NAMES_DELIMITER = ", ";
+    private static final String INIT_MESSAGE_FORMAT = "\n%s 에게 카드 2장을 나누어 줍니다.\n";
+    private static final String CURRENT_STATE_MESSAGE_FORMAT = "\n%s의 카드\n";
 
     private OutputView() {
     }
 
     public static void guide() {
         System.out.println(GUIDE_MESSAGE);
+    }
+
+    public static void printInit(List<Player> players) {
+        String names = players.stream()
+            .map(Player::getName)
+            .map(Name::toString)
+            .collect(joining(NAMES_DELIMITER));
+
+        System.out.printf(INIT_MESSAGE_FORMAT, names);
+    }
+
+    public static void printCurrentState(Player player) {
+        System.out.printf(CURRENT_STATE_MESSAGE_FORMAT, player.getName());
+        Cards cards = player.getCards();
+
+        cards.getCards().forEach(System.out::println);
     }
 }
