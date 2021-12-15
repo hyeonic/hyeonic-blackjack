1ff44e7ebc0f5d4d469c1eacf0f63948ba314dd5
hyeonic <dev.hyeonic@gmail.com>
2021-12-15 20:01:04 +0900

    feat: 게이머의 이름을 표현하기 위한 Name 추가

--- src/main/java/blackjack/domain/player/Name.java
+++ src/main/java/blackjack/domain/player/Name.java
@@ -1,2 +1,18 @@
-package blackjack.domain.player;public class Name {
+package blackjack.domain.player;
+
+public class Name {
+    public static final int MAX_NAME_LENGTH = 5;
+
+    private final String name;
+
+    public Name(String name) {
+        validateLength(name);
+        this.name = name;
+    }
+
+    private void validateLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
