7212b2cfa0f3dfe2de6f881b7ac2ddb6a4ab27a5
hyeonic <dev.hyeonic@gmail.com>
2021-12-15 20:44:25 +0900

    feat: 트럼프 카드를 나타내기 위한 Card 추가

---
+++ src/main/java/blackjack/domain/card/Card.java
@@ -0,0 +1,16 @@
+package blackjack.domain.card;
+
+public class Card {
+    private final Pattern pattern;
+    private final Number number;
+
+    public Card(Pattern pattern, Number number) {
+        this.pattern = pattern;
+        this.number = number;
+    }
+
+    public Card(Card card) {
+        this.pattern = card.pattern;
+        this.number = card.number;
+    }
+}
