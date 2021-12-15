b86cc082cf0d455a0b54eb579f5e1ea00b1977dd
hyeonic <dev.hyeonic@gmail.com>
2021-12-15 19:59:14 +0900

    feat: 입력을 위한 InputView 추가

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,2 +1,13 @@
-package blackjack.view;public class InputView {
+package blackjack.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private InputView() {
+    }
+
+    public static String name() {
+        return Console.readLine();
+    }
 }
